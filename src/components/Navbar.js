a4ada02d50507913f4721baef76a62453a6063fc
Ksoejati <ksujatirianto@gmail.com>
2022-07-20 22:29:52 +0700

    Menambahkan fitur navbar fix dan blur effect

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Navbar = () => {
   const [hamburgerRotate, setHamburgerRotate] = useState(false);
+  const [fixNavbar, setFixNavbar] = useState("");
 
   function addClassName() {
     setHamburgerRotate(!hamburgerRotate);
   }
 
+  const checkScroll = () => {
+    if (window.scrollY > 0) {
+      setFixNavbar(
+        "fixed z-[9999] bg-white bg-opacity-70 backdrop-blur-sm box-shadow: inset 0 -1px 0 0 rgba(0, 0, 0, 0.1"
+      );
+    } else {
+      setFixNavbar("absolute z-10");
+    }
+  };
+
+  window.addEventListener("scroll", checkScroll);
+
   return (
-    <div className="bg-transparent absolute top-0 left-0 w-full flex items-center z-10">
+    <div
+      id="navbar"
+      className={`bg-transparent top-0 left-0 w-full flex items-center ${fixNavbar}`}
+      // className="bg-transparent top-0 left-0 w-full flex items-center fixed z-[9999]"
+    >
       <div className="container">
         <div className="flex item-center justify-between">
           <div className="px-4">
